e00967ffc548e78dd4e9786a1dc956f6023930f8
Difarem <difarem@gmail.com>
2018-03-24 19:31:47 +0100

    cant think of a good commit name

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,5 +1,6 @@
 extern crate discord_rpc;
 
+use std::time::SystemTime;
 use discord_rpc::{RPC, EventHandlers, RichPresence};
 
 const APPLICATION_ID: &'static str = "378906438590005272";
@@ -10,14 +11,29 @@ impl EventHandlers for Handlers {
     fn ready() {
         println!("We're ready!");
     }
+
+    fn errored(errcode: i32, message: &str) {
+        println!("Error {}: {}", errcode, message);
+    }
+
+    fn disconnected(errcode: i32, message: &str) {
+        println!("Disconnected {}: {}", errcode, message);
+    }
 }
 
 fn main() {
     let rpc = RPC::init::<Handlers>(APPLICATION_ID, false, None).unwrap();
 
     let presence = RichPresence {
-        state: Some("IT motherFUCKING".to_string()),
-        details: Some("works im CRYING".to_string()),
+        details: Some("IT works".to_string()),
+        state: Some("im CRYING".to_string()),
+        start_time: Some(SystemTime::now()),
+        large_image_key: Some("kotori".to_string()),
+        large_image_text: Some("blerghfdsg".to_string()),
+        small_image_key: Some("jhon_egbert".to_string()),
+        small_image_text: Some("John Egbert".to_string()),
+        party_size: Some(4),
+        party_max: Some(13),
         ..Default::default()
     };
     rpc.update_presence(presence).unwrap();
--- src/presence.rs
+++ src/presence.rs
@@ -1,4 +1,4 @@
-use std::time::SystemTime;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Default, Clone, Hash, PartialEq)]
 pub struct RichPresence {
@@ -44,4 +44,70 @@ pub struct RichPresence {
 
     /// Unique hashed string for chat invitations and Ask to Join. Maximum of 128 bytes.
     pub join_secret: Option<String>,
+}
+
+use sys;
+use libc;
+use std::ffi::{CString, NulError};
+use std::ptr;
+
+impl RichPresence {
+    pub(crate) fn to_sys(self) -> Result<sys::DiscordRichPresence, NulError> {
+        Ok(sys::DiscordRichPresence {
+            state: match self.state {
+                None => ptr::null(),
+                Some(state) => CString::new(state.clone())?.into_raw(),
+            },
+            details: match self.details {
+                None => ptr::null(),
+                Some(details) => CString::new(details)?.into_raw(),
+            },
+            startTimestamp: match self.start_time {
+                None => 0,
+                Some(time) => time.duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
+            },
+            endTimestamp: match self.end_time {
+                None => 0,
+                Some(time) => time.duration_since(UNIX_EPOCH).unwrap().as_secs() as i64,
+            },
+            largeImageKey: match self.large_image_key {
+                None => ptr::null(),
+                Some(key) => CString::new(key)?.into_raw(),
+            },
+            largeImageText: match self.large_image_text {
+                None => ptr::null(),
+                Some(text) => CString::new(text)?.into_raw(),
+            },
+            smallImageKey: match self.small_image_key {
+                None => ptr::null(),
+                Some(key) => CString::new(key)?.into_raw(),
+            },
+            smallImageText: match self.small_image_text {
+                None => ptr::null(),
+                Some(text) => CString::new(text)?.into_raw(),
+            },
+            partyId: match self.party_id {
+                None => ptr::null(),
+                Some(id) => CString::new(id)?.into_raw(),
+            },
+            partySize: match self.party_size {
+                None => 0,
+                Some(size) => size as libc::c_int,
+            },
+            partyMax: match self.party_max {
+                None => 0,
+                Some(max) => max as libc::c_int,
+            },
+            matchSecret: ptr::null(), // deprecated
+            joinSecret: match self.join_secret {
+                None => ptr::null(),
+                Some(secret) => CString::new(secret)?.into_raw(),
+            },
+            spectateSecret: match self.spectate_secret {
+                None => ptr::null(),
+                Some(secret) => CString::new(secret)?.into_raw(),
+            },
+            instance: 0, // deprecated
+        })
+    }
 }
