c81787aaa4f794d41b6df07b6d5c47da2caae8ad
Difarem <difarem@gmail.com>
2018-03-24 18:32:36 +0100

    uhhhh progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,95 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate discord_rpc_sys as sys;
+extern crate libc;
+
+mod presence;
+pub use presence::RichPresence;
+
+pub struct JoinRequest {
+    pub user_id: String,
+    pub username: String,
+    pub discriminator: String,
+    pub avatar: String,
+}
+
+pub trait EventHandlers {
+    fn ready() {
+    }
+
+    fn errored(_errcode: i32, _message: &str) {
+    }
+
+    fn disconnected(_errcode: i32, _message: &str) {
+    }
+
+    fn join_game(_secret: &str) {
+    }
+
+    fn spectate_game(_secret: &str) {
+    }
+
+    fn join_request(_request: JoinRequest) {
+    }
+}
+
+pub struct RPC;
+
+use std::ffi::{CStr, CString, NulError};
+use std::ptr;
+
+impl RPC {
+    pub fn init<EH: EventHandlers>(app_id: &str, auto_register: bool, steam_id: Option<&str>) -> Result<RPC, NulError> {
+        extern "C" fn ready_wrapper<EH: EventHandlers>() {
+            EH::ready();
+        }
+        unsafe extern "C" fn disconnected_wrapper<EH: EventHandlers>(errcode: libc::c_int, message: *const libc::c_char) {
+            EH::disconnected(errcode as i32, &CStr::from_ptr(message).to_string_lossy());
+        }
+        unsafe extern "C" fn errored_wrapper<EH: EventHandlers>(errcode: libc::c_int, message: *const libc::c_char) {
+            EH::errored(errcode as i32, &CStr::from_ptr(message).to_string_lossy());
+        }
+        unsafe extern "C" fn join_game_wrapper<EH: EventHandlers>(secret: *const libc::c_char) {
+            EH::join_game(&CStr::from_ptr(secret).to_string_lossy());
+        }
+        unsafe extern "C" fn spectate_game_wrapper<EH: EventHandlers>(secret: *const libc::c_char) {
+            EH::spectate_game(&CStr::from_ptr(secret).to_string_lossy());
+        }
+        unsafe extern "C" fn join_request_wrapper<EH: EventHandlers>(join_request: *const sys::DiscordJoinRequest) {
+            let req = JoinRequest {
+                user_id: CStr::from_ptr((*join_request).userId).to_string_lossy().into_owned(),
+                username: CStr::from_ptr((*join_request).username).to_string_lossy().into_owned(),
+                discriminator: CStr::from_ptr((*join_request).discriminator).to_string_lossy().into_owned(),
+                avatar: CStr::from_ptr((*join_request).avatar).to_string_lossy().into_owned(),
+            };
+            EH::join_request(req);
+        }
+
+        let mut sys_handlers = sys::DiscordEventHandlers {
+            ready: Some(ready_wrapper::<EH>),
+            disconnected: Some(disconnected_wrapper::<EH>),
+            errored: Some(errored_wrapper::<EH>),
+            joinGame: Some(join_game_wrapper::<EH>),
+            spectateGame: Some(spectate_game_wrapper::<EH>),
+            joinRequest: Some(join_request_wrapper::<EH>),
+        };
+
+        unsafe {
+            sys::Discord_Initialize(CString::new(app_id)?.into_raw(),
+                &mut sys_handlers,
+                auto_register as libc::c_int,
+                match steam_id {
+                    None => ptr::null(),
+                    Some(id) => CString::new(id)?.into_raw(),
+                });
+        }
+
+        Ok(RPC)
+    }
+}
+
+impl Drop for RPC {
+    fn drop(&mut self) {
+        unsafe {
+            sys::Discord_Shutdown();
+        }
     }
 }
---
+++ src/presence.rs
@@ -0,0 +1,46 @@
+use std::time::Duration;
+
+pub struct RichPresence {
+    /// The user's current party status. Maximum of 128 bytes.
+    ///
+    /// For example: `"Looking to Play"`, `"Playing Solo"`, `"In a Group"`...
+    pub state: Option<String>,
+
+    /// What the player is currently doing. Maximum of 128 bytes.
+    ///
+    /// For example: `"Competitive - Captain's Mode"`, `"In Queue"`, `"Unranked PvP"`...
+    pub details: Option<String>,
+
+    /// Time since game start. Including will show time as "elapsed".
+    pub start_time: Option<Duration>,
+
+    /// Time for game end. Including will show time as "remaining".
+    pub end_time: Option<Duration>,
+
+    /// Name of the uploaded image for the large profile artwork. Maximum of 32 bytes.
+    pub large_image_key: Option<String>,
+
+    /// Tooltip for the large image. Maximum of 128 bytes.
+    pub large_image_text: Option<String>,
+
+    /// Name of the uploaded image for the large profile artwork. Maximum of 32 bytes.
+    pub small_image_key: Option<String>,
+
+    /// Tooltip for the large image. Maximum of 128 bytes.
+    pub small_image_text: Option<String>,
+
+    /// ID of the player's party, lobby, or group. Maximum of 128 bytes.
+    pub party_id: Option<String>,
+
+    /// Current size of the player's party, lobby, or group.
+    pub party_size: Option<u32>,
+
+    /// Maximum size of the player's party, lobby, or group.
+    pub party_max: Option<u32>,
+
+    /// Unique hashed string for Spectate button. Maximum of 128 bytes.
+    pub spectate_secret: Option<String>,
+
+    /// Unique hashed string for chat invitations and Ask to Join. Maximum of 128 bytes.
+    pub join_secret: Option<String>,
+}
